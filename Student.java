19fadd5be04b9e4c3988fdce5a53decdd64e6318
Negbwei <130391089+Negbwei@users.noreply.github.com>
2024-04-30 17:01:24 +0100

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,8 +1,8 @@
 package User;
 
 public class Student extends User {
-    String matric_no = "bhu/22/04/05/0054";
-    String admission_no = "1789237958103";
+    String matric_no = "bhu/22/04/05/0050";
+    String admission_no = "9087542134678";
     String registerCourse() {
         if (matric_no.equals("") || admission_no.equals("")) {
             return "You cannot register at this moment";
@@ -14,7 +14,7 @@ public class Student extends User {
         if (matric_no.equals("") || admission_no.equals("")) {
             return "Cannot check your result. Are you even a student?";
         } else {
-            return "You got 6As and 1B";
+            return "You got 7As and 3Bs";
         }
     }    
 }
